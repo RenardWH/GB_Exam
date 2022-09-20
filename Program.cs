b55ceb3f0966b5ae5b972ce8f39276f654e59857
Renard_WH <rybochkin116@gmail.com>
2022-09-21 00:14:24 +0400

    second  commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите количество слов в вашем массиве");
+int size = Convert.ToInt32(Console.ReadLine());
+string [] array = new string [size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите  {i+1} слово массива");
+    array[i] = Convert.ToString(Console.ReadLine());
+}
+Console.WriteLine("Ваш массив");
+Console.WriteLine('[' + string.Join(" , ",array)+']');
+
+int sizeResultArray = 0;
+
+for (int i = 0; i < size; i++)
+{
+    if(array[i].Length <4)
+    {
+        sizeResultArray++;
+    }
+}
+string [] resultArray = new string [sizeResultArray];
+int Counter = 0;
+for (int i = 0; i < size; i++)
+{
+    if(array[i].Length <4)
+    {
+        resultArray[Counter] = array[i];
+        Counter++;
+    }
+}
+Console.WriteLine("Массив из слов, длина которых меньше, либо равна 3 символам ");
+Console.WriteLine('[' + string.Join(" , ",resultArray)+']');
